8b9b1c9c482e2bb587999a5f0af9f8d21bcad85e
Roman Kiselev <roman.kiselew@gmail.com>
2014-09-24 17:28:12 +0200

    All right, first working version!

--- main.py
+++ main.py
@@ -6,21 +6,45 @@ import xcal_raman as xcal
 import winspec
 import PyZenity as pz
 import os
+import sys
 import ConfigParser as cp
+import pickle # TODO use it!
 
+fname = sys.argv[1]
+print sys.argv[1]
+#fname = "Cyclohexan1.SPE"
 if os.path.exists(".speview.conf"): # We have found config, lets use it!
     config = cp.SafeConfigParser()
     config.read(".speview.conf")
     if config.get("general", "wavenum_calibration") == "yes" :
         material = config.get("wavenum_calibration", "material")
-        datafile = config.get("wavenum_calibration", "datafile")
+        xcalfile = config.get("wavenum_calibration", "datafile")
         darkfile = config.get("wavenum_calibration", "darkfile")
         shift    = config.getint("wavenum_calibration", "shift")
-        cal_f, p = xcal.calibrate_spe(datafile, darkfile, material=material,
+        cal_f, p = xcal.calibrate_spe(xcalfile, darkfile, material=material,
                                       figure=pl.figure(), shift=shift)
         pl.savefig("calibration_report-" + material + ".pdf")
+        pl.close(pl.gcf())
         # TODO: use pickle to save cal_f
         calibrated = True
+    
+    # We have calibration function, lets plot the spectrum!
+    spec = winspec.Spectrum(fname)
+    if   os.path.exists(fname[:-3] + "dark.SPE"):  # it overrides config
+        spec.background_correct(fname[:-3] + "dark.SPE")
+    elif os.path.exists(fname[:-3] + "dark.spe"):
+        spec.background_correct(fname[:-3] + "dark.spe")
+    else:
+        spec.background_correct(config.get("general", "darkfile"))
+
+    pl.plot(cal_f(spec.wavelen), spec.lum)
+    if calibrated:
+        pl.xlabel("Wavenumber, cm$^{-1}$")
+    else:
+        pl.xlabel("pixel number")
+    pl.ylabel("Counts")
+    pl.title(fname)
+    pl.show()
     
 else: 
     ans = pz.Question("Should I just show the SPE file?\n" +
@@ -30,8 +54,6 @@ else:
     if ans:
         pz.InfoMessage("You selected yes")
     
-pl.plot([],[])
-pl.show()
 #cal_f, p = xcal.calibrate_spe("polystyrene.SPE", "dark.SPE",
 #                              material="polystyrene", figure=figure())
 #savefig("xcal-report-polystyrene.pdf")
