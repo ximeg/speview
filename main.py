14c9fa53132d51da2b7a945fbe07a526d881897c
Roman Kiselev <roman.kiselew@gmail.com>
2014-09-24 18:12:22 +0200

    Code cleanup. Implemented check if the bg correction should be used or not

--- main.py
+++ main.py
@@ -32,15 +32,17 @@ if os.path.exists(".speview.conf"): # We have found config, lets use it!
             np.savetxt("xcal_coeffs.csv", p)
         calibrated = True
     
-    # We have a calibration function, lets plot the spectrum!
+    # Make dark current correction
     spec = winspec.Spectrum(fname)
-    if   os.path.exists(fname[:-3] + "dark.SPE"):  # it overrides config
-        spec.background_correct(fname[:-3] + "dark.SPE")
-    elif os.path.exists(fname[:-3] + "dark.spe"):
-        spec.background_correct(fname[:-3] + "dark.spe")
-    else:
-        spec.background_correct(config.get("general", "darkfile"))
+    if config.get("general", "use_dark") == "yes":
+        if   os.path.exists(fname[:-3] + "dark.SPE"):  # it overrides config
+            spec.background_correct(fname[:-3] + "dark.SPE")
+        elif os.path.exists(fname[:-3] + "dark.spe"):
+            spec.background_correct(fname[:-3] + "dark.spe")
+        else:
+            spec.background_correct(config.get("general", "darkfile"))
 
+    # Make a plot
     if calibrated:
         pl.plot(cal_f(spec.wavelen), spec.lum)
         pl.xlabel("Wavenumber, cm$^{-1}$")
@@ -52,21 +54,16 @@ if os.path.exists(".speview.conf"): # We have found config, lets use it!
     pl.ylabel("Counts")
     pl.title(fname)
     pl.show()
-    
-else: 
+
+
+else:
     ans = pz.Question("Should I just show the SPE file?\n" +
                      "If you answer 'No', then I will\n" +
-                     "create a config with standard\n" +
+                     "create a config with the standard\n" +
                      "settings for this folder\n")
     if ans:
         pz.InfoMessage("You selected yes")
     
-#cal_f, p = xcal.calibrate_spe("polystyrene.SPE", "dark.SPE",
-#                              material="polystyrene", figure=figure())
-#savefig("xcal-report-polystyrene.pdf")
-# We have calibration function, lets plot spectrum of some substance
-#s = winspec.Spectrum("substance.SPE")
-#s.background_correct("dark.SPE")
-#plot(cal_f(s.wavelen), s.lum)
-#xlabel("Wavenumber, cm$^{-1}$")
-#ylabel("Counts")
+
+
+
