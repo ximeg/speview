8df9ee3ef85fa584eb808421d6faaa06ca9b999c
Roman Kiselev <roman.kiselew@gmail.com>
2014-09-26 13:01:13 +0200

    Got rid of unneccessary lines

--- qt4example.py
+++ qt4example.py
@@ -6,17 +6,15 @@ import numpy as np
 from matplotlib.figure import Figure
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.backends.backend_qt4agg import (
-    FigureCanvasQTAgg as FigureCanvas,
-    NavigationToolbar2QT as NavigationToolbar)
+                                  FigureCanvasQTAgg as FigureCanvas,
+                                  NavigationToolbar2QT as NavigationToolbar)
 from matplotlib.backends import qt4_compat
+
+
 use_pyside = qt4_compat.QT_API == qt4_compat.QT_API_PYSIDE
 
-if use_pyside:
-    from PySide.QtCore import *
-    from PySide.QtGui import *
-else:
-    from PyQt4.QtCore import *
-    from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
 
 class AppForm(QMainWindow):
