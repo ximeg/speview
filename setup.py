91460ad3197dd43185545a6835e5d7cecfe8be2b
Roman Kiselev <roman.kiselew@gmail.com>
2014-09-30 21:12:07 +0200

    I have figured out that scipy>=0.11.0 is required, otherwise function argrelmin is not available

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='speview',
-    version='0.1.3',
+    version='0.1.4',
     author='Roman Kiselev',
     author_email='roman.kiselew@gmail.com',
     scripts=['speview.py'],
@@ -12,10 +12,9 @@ setup(
     description='Program to display binary SPE files with Raman spectra',
     long_description=open('README.md').read(),
     install_requires=[
-        "PySide",
         "numpy",
         "matplotlib",
-        "scipy",
+        "scipy>=0.11.0",
         "xcal-raman>=0.1.5",
         "PyZenity>=0.1.4"
         ]
