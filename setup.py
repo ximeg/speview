65053e29624364ab46cd180764f85479d68a4e12
Roman Kiselev <roman.kiselew@gmail.com>
2014-09-30 17:34:08 +0200

    Increased version number in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='speview',
-    version='0.1.0',
+    version='0.1.2',
     author='Roman Kiselev',
     author_email='roman.kiselew@gmail.com',
     scripts=['speview.py'],
