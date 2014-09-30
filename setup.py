65949785b9439a7bd2ccf7632edc1cf404288e1e
Roman Kiselev <roman.kiselew@gmail.com>
2014-09-30 20:37:28 +0200

    I removed automatically added dependecies and removed version specification

--- setup.py
+++ setup.py
@@ -12,18 +12,12 @@ setup(
     description='Program to display binary SPE files with Raman spectra',
     long_description=open('README.md').read(),
     install_requires=[
-        "argparse>=1.2.1",
-        "mock>=1.0.1",
-        "nose>=1.3.4",
-        "pyparsing>=2.0.2",
-        "python-dateutil>=2.2",
-        "six>=1.8.0",
-        "wsgiref>=0.1.2",
-        "PySide>=1.2.2",
-        "numpy>=1.9.0",
-        "matplotlib>=1.4.0",
-        "scipy>=0.14.0",
-        "xcal-raman>=0.1.5"
+        "PySide",
+        "numpy",
+        "matplotlib",
+        "scipy",
+        "xcal-raman>=0.1.5",
+        "PyZenity>=0.1.4"
         ]
 )
 
